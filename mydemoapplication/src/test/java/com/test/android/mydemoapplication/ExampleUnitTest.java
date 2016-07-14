db2ac21a22068ece0c4691f8e37beb940f5c1c40
zcychong <zcychong7289610>
2016-07-14 14:06:56 +0800

    craate

---
+++ mydemoapplication/src/test/java/com/test/android/mydemoapplication/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.test.android.mydemoapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
